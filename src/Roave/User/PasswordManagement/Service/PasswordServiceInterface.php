60bc472e59b91bcaa9745e31919bf8e6bd847414
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2014-11-22 23:12:53 +0100

    Prototyping the module

---
+++ src/Roave/User/PasswordManagement/Service/PasswordServiceInterface.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright (c) 2014 Roave, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @copyright 2014 Roave, LLC
+ * @license http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+
+namespace Roave\User\PasswordManagement\Service;
+
+use Roave\NonceUtility\Entity\NonceEntity;
+use Roave\User\Core\Entity\UserEntityInterface;
+
+interface PasswordServiceInterface
+{
+    /**
+     * Update a users password
+     *
+     * @triggers roave:user:password:update.pre
+     * @triggers roave:user:password:update.post
+     *
+     * @param string              $password
+     * @param UserEntityInterface $user
+     *
+     * @return bool
+     */
+    public function update($password, UserEntityInterface $user);
+
+    /**
+     * Update a user authorized using a nonce from the reset password process.
+     *
+     * @param string              $password
+     * @param UserEntityInterface $user
+     * @param NonceEntity         $nonce
+     *
+     * @return bool
+     */
+    public function updateWithNonceAsAuthorization($password, UserEntityInterface $user, NonceEntity $nonce);
+
+    /**
+     * Generate a random password for the user and return it.
+     *
+     * @triggers roave:user:password:reset.pre
+     * @triggers roave:user:password:reset.post
+     *
+     * @param UserEntityInterface $user
+     *
+     * @return string
+     */
+    public function reset(UserEntityInterface $user);
+}
